d147b297eb2a4929032bce334d294ebb2ad10b5e
Kevin Zheng <kevinzheng813@gmail.com>
2021-02-05 22:23:24 -0500

    Player v1

---
+++ Player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, name, age, country, status, site, info):
+        this.name = name
+        this.age = age
+        this.country = country
+        this.status = status
+        this.site = site
+        this.info = info
