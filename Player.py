a8df2d405f59b6463e8cb07bdda0d0a3e593b08e
Eric Chen <echen2018@hotmail.com>
2021-02-06 17:00:45 -0500

    fixed self

--- Player.py
+++ Player.py
@@ -1,8 +1,8 @@
 class Player:
     def __init__(self, name, age, country, status, site, info):
-        this.name = name
-        this.age = age
-        this.country = country
-        this.status = status
-        this.site = site
-        this.info = info
+        self.name = name
+        self.age = age
+        self.country = country
+        self.status = status
+        self.site = site
+        self.info = info
