165a77aa8731a7a0b0c25685ed1f55aae02ef204
Kevin Zheng <kevinzheng813@gmail.com>
2021-02-07 07:44:07 -0500

    USTA v0.5

---
+++ getUSTAData.py
@@ -0,0 +1,24 @@
+from Player import Player
+import requests
+import json
+import pprint as pp
+from bs4 import BeautifulSoup
+import binascii
+import pandas as pd
+import json
+from common_functions import *
+import csv
+
+U14
+https://static.dwcdn.net/data/1Ci8B.csv?v=1612701780000
+
+def get_U16_USTA_Data(masterDict):
+    data = pd.read_csv("https://static.dwcdn.net/data/0M24X.csv?v=1612697340000")
+    print(data[1])
+U18
+https://static.dwcdn.net/data/AAkQ3.csv?v=1612701600000
+def get_USTA_Data(masterDict):
+    get_U18_USTA_Data(masterDict)
+
+testDict = dict()
+get_USTA_Data(testDict)
