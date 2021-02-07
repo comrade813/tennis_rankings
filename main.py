daf9748dc03cbe18018266428e5ae2ca3e272b21
Kevin Zheng <kevinzheng813@gmail.com>
2021-02-07 08:06:11 -0500

    main v1.0

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import getUTRData as UTR
 import getITFData as ITF
 import pprint as pp
+import xlwt
+from xlwt import Workbook
 
+wb = Workbook()
 masterDict = {}
-def tempMain(playerDict):
-    UTR.getUTRData(playerDict)
-    ITF.getITFData(playerDict)
-    return playerDict
 
-tempMain(masterDict)
+def getData():
+    UTR.getUTRData(masterDict)
+    ITF.getITFData(masterDict)
 
-# for player in masterDict.values():
-#     print(player.name + ", Age: " + str(player.age) + ", Country: " + player.country + ", status: " + player.status + ", site: " + str(player.site) + ", info: " + str(player.info) + "\n")
+def print_player(out, name, row):
+    out.write(row,0,name)
+    out.write(row,1,masterDict[name].age)
+    out.write(row,2,masterDict[name].country)
+    out.write(row,3,masterDict[name].status)
+    if "UTR" in masterDict[name].site.keys():
+        out.write(row,4,masterDict[name].site["UTR"])
+    if "ITF" in masterDict[name].site.keys():
+        out.write(row,5,masterDict[name].site["ITF"])
+    if "USTA" in masterDict[name].site.keys():
+        out.write(row,6,masterDict[name].site["USTA"])
 
-# print(len(masterDict))
+def generate_spreadsheet():
+    out = wb.add_sheet("Sheet 1")
+    out.write(0,0,"NAME")
+    out.write(0,1,"AGE")
+    out.write(0,2,"COUNTRY")
+    out.write(0,3,"STATUS")
+    out.write(0,4,"UTR")
+    out.write(0,5,"ITF")
+    out.write(0,6,"USTA")
+
+    row = 0
+    for name in masterDict.keys():
+        print_player(out, name, row)
+        row += 1
